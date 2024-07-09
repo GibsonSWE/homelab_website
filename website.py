0d02d33156d135ed90fca26e5040214b419a02a8
Jacob Henriksen <jacob.henriksen94@gmail.com>
2024-07-09 16:15:19 +0200

    Added some more pages

--- website.py
+++ website.py
@@ -19,5 +19,29 @@ def resources():
 def cml():
     return render_template("cml.html")
 
+@app.route("/about_me")
+def about_me():
+    return render_template("about_me.html")
+
+
+# HOMELAB DROPDOWN
+@app.route("/homelab/overview")
+def homelab_overview():
+    return render_template("/homelab/overview.html")
+
+@app.route("/homelab/components")
+def homelab_components():
+    return render_template("/homelab/components.html")
+
+@app.route("/homelab/network")
+def homelab_network():
+    return render_template("/homelab/network.html")
+
+@app.route("/homelab/future_plans")
+def homelab_future_plans():
+    return render_template("/homelab/future_plans.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
